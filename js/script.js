77da6b7d89b22133a914d4ced6ea84226f98f01f
Awani <awani.rawat@gmail.com>
2020-05-25 21:10:24 +0530

    Designing home screen

--- js/script.js
+++ js/script.js
@@ -1,13 +1,5 @@
 //click event in jquery is click unlike js where it is onclick
 
-$(document).ready(function(){
-    $('h1').click(function(){
-        $('#main').css({
-            color:'red',
-            fontSize: 20
-        });
-    });    
+$(function(){
+    $('#preloader').delay(500).fadeOut();    
 });
-
-
-$(window).on("load", function(){});
