11a8e4c7b253c2003487d8fda9f15a256a31ce54
Awani <awani.rawat@gmail.com>
2020-05-28 20:56:31 +0530

    Added statement, services section

--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
 //click event in jquery is click unlike js where it is onclick
 
-$(function(){
-    $('#preloader').delay(500).fadeOut();    
+$(function () {
+    $('#preloader').delay(500).fadeOut();
+    $("#team-members").owlCarousel({
+        items: 2,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true,
+        nav: true,
+        dots: false,
+        navText: ['<i class="fa fa-angle-left">', '<i class="fa fa-angle-right">']
+    });
+    $("#progress-elements").waypoint(function () {
+        $(".progress-bar").each(function () {
+            $(this).animate({
+                width: $(this).attr("aria-valuenow") + "%"
+            }, 1000);
+        });
+        this.destroy();
+    }, {
+        offset: 'bottom-in-view'
+    });
 });
